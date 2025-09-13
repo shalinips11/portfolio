de0dcd52c7cc99b84d54fd47db2fa02159503a30
shalinips11 <shalinipalanisamy11@gmail.com>
2025-09-13 21:54:45 +0530

    Fix case sensitivity for MyWork import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import  About from './Components/About/About'
 import Service from './Components/Services/Service'
-import Mywork from './Components/Mywork/Mywork'
+import Mywork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
